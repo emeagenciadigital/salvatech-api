be2fc3aac8629bd28ac5c2766780f775df7d0579
leonel122 <inf.leonel@gmail.com>
2023-03-04 08:30:44 -0500

    ADD: se terminan los permisos

--- src/hooks/abilities.js
+++ src/hooks/abilities.js
@@ -1,20 +1,16 @@
 const {AbilityBuilder, createMongoAbility, Ability} = require('@casl/ability');
-const {createAliasResolver, makeAbilityFromRules} = require('feathers-casl');
 
 const {toMongoQuery} = require('@casl/mongoose');
 const {Forbidden} = require('@feathersjs/errors');
-const {Logform} = require('winston');
-const {Professions} = require('../services/professions/professions.class');
-const {Tasks} = require('../services/tasks/tasks.class');
 const TYPE_KEY = Symbol.for('type');
 
 const READ = 'read';
 const UPDATE = 'update';
 const DELETE = 'delete';
 const CREATE = 'create';
-const USERS = 'users';
-const MANAGE = 'MANAGE';
+const MANAGE = 'manage';
 
+const USERS = 'users';
 const COMPANY_USERS = 'company-users';
 const COMPANY_USERS_WISH_LIST = 'company-users-wish-list.model';
 const PROFESSIONS = 'professions';
@@ -23,6 +19,13 @@ const USER_SKILLS = 'users-skills';
 const USERS_EDUCATIONS = 'users-educations';
 const TASK = 'task';
 const TASK_TIME_TRACKING = 'task-time-tracking';
+const TASK_FILES = 'task-files';
+const PROJECT_USERS = 'project-users';
+const PROJECT = 'project';
+const REQUEST_MEMBERS = 'request-members';
+const STAND_UP = 'stand-up';
+const SUPPORT_TICKETS = 'support-tickets';
+const USER_WORK_EXPERIENCE = 'user-work-experience';
 
 function subjectName(subject) {
   if (!subject || typeof subject === 'string') {
@@ -38,11 +41,27 @@ const getCompanies = ({context, user_id}) =>
     .getModel()
     .query()
     .where({user_id: user_id, deletedAt: null})
-    .then((it) => it[0]);
+    .then((it) => it);
+
+const getRequestMembers = ({context, company_id}) =>
+  context.app
+    .service('request-members')
+    .getModel()
+    .query()
+    .where({company_id: company_id, deletedAt: null})
+    .then((it) => it);
+
+const getCompanyOnboardings = ({context, company_id}) =>
+  context.app
+    .service('company-onboardings')
+    .getModel()
+    .query()
+    .where({company_id: company_id, deletedAt: null})
+    .then((it) => it);
 
 const getMyTasks = ({context, user_id}) =>
   context.app
-    .service('task')
+    .service('tasks')
     .getModel()
     .query()
     .where({user_id: user_id, deletedAt: null})
@@ -63,26 +82,83 @@ async function defineAbilitiesFor(user, context) {
 
   if (user) {
     can(READ, [PROFESSIONS, SKILLS]);
-    can(MANAGE, [COMPANY_USERS_WISH_LIST, USER_SKILLS, USERS_EDUCATIONS], {
-      user_id: user.id,
-    });
+    can(
+      MANAGE,
+      [
+        COMPANY_USERS_WISH_LIST,
+        USER_SKILLS,
+        USERS_EDUCATIONS,
+        USER_WORK_EXPERIENCE,
+      ],
+      {
+        user_id: user.id,
+      },
+    );
     can([READ, UPDATE], [TASK], {user_id: user.id});
 
-    can([CREATE], [TASK_TIME_TRACKING]);
+    can([MANAGE], [TASK_TIME_TRACKING], {user_id: user.id});
 
-    const myTasks = await getMyTasks({context, user_id: user.id});
+    can([MANAGE], [USERS], {id: user.id});
+    can([MANAGE], [PROJECT, TASK], {created_by_user_id: user.id});
+
+    can([READ], [PROJECT_USERS], {user_id: user.id});
+
+    const myTasksIds = (await getMyTasks({context, user_id: user.id})).map(
+      (it) => it.id,
+    );
+
+    can(MANAGE, [TASK_FILES], {task_id: {$in: myTasksIds}});
   }
 
   const companiesUsers = await getCompanies({context, user_id: user.id});
 
-  companiesUsers.map((companyUser) => {
-    if (companyUser.owner === 'true') {
+  for (const companyUser of companiesUsers) {
+    if (companyUser.role === 'admin') {
       //cosas que puede hacer el owner
-      can([MANAGE], [COMPANY_USERS, COMPANY_USERS_WISH_LIST], {
-        company_id: companyUser.company_id,
+      can(
+        [MANAGE],
+        [
+          COMPANY_USERS,
+          COMPANY_USERS_WISH_LIST,
+          PROJECT_USERS,
+          PROJECT,
+          REQUEST_MEMBERS,
+          STAND_UP,
+          SUPPORT_TICKETS,
+          TASK,
+        ],
+        {
+          company_id: companyUser.company_id,
+        },
+      );
+
+      const onboardingsIds = (
+        await getCompanyOnboardings({
+          context,
+          company_id: companyUser.company_id,
+        })
+      ).map((it) => it.id);
+
+      const requestMembersIds = (
+        await getRequestMembers({
+          context,
+          company_id: companyUser.company_id,
+        })
+      ).map((it) => it.id);
+
+      can(
+        MANAGE,
+        ['company-onboardings-meetings', 'company-onboarding-files'],
+        {
+          company_onboarding_id: {$in: [onboardingsIds]},
+        },
+      );
+
+      can(MANAGE, ['requests-members-skills'], {
+        request_member_id: {$in: requestMembersIds},
       });
     }
-  });
+  }
 
   // if (process.env.NODE_ENV !== 'production') {
   //   // can('create', ['users']);
