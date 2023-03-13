f3a6bd7e18928fec8a104fa856397d394e16d353
leonel122 <inf.leonel@gmail.com>
2023-03-13 10:11:29 -0500

    ADD: se arregla bug en feedback.model.js

--- src/services/feedback/feedback.hooks.js
+++ src/services/feedback/feedback.hooks.js
@@ -1,7 +1,26 @@
 const registerFeedBack = require('./hooks/register-feedback');
-const {disallow} = require('feathers-hooks-common');
+const {disallow, fastJoin} = require('feathers-hooks-common');
 const tree = require('./hooks/tree');
 const {paramsFromClient} = require('feathers-hooks-common');
+
+const joinsResolves = {
+  joins: {
+    join: () => async (records, context) => {
+      const getIfHaveParentId = ({parent_id}) =>
+        context.app
+          .service('feedback')
+          .getModel()
+          .query()
+          .where({parent_id, deletedAt: null})
+          .limit(1)
+          .then((it) => it[0]);
+
+      const haveParentId = await getIfHaveParentId({parent_id: records.id});
+
+      records.have_reply = !!haveParentId;
+    },
+  },
+};
 
 module.exports = {
   before: {
@@ -15,7 +34,7 @@ module.exports = {
   },
 
   after: {
-    all: [],
+    all: [fastJoin(joinsResolves)],
     find: [tree()],
     get: [],
     create: [],
