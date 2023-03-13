d2669754aac57500c74488cd04c4d90579a33b4b
leonel122 <inf.leonel@gmail.com>
2023-03-13 10:21:42 -0500

    ADD: se arregla bug en feedback.model.js

--- src/services/companies/companies.hooks.js
+++ src/services/companies/companies.hooks.js
@@ -1,6 +1,6 @@
 const searchAdminByQ = require('./hooks/search-admin-by-q');
 const removeSoftDelete = require('../../hooks/remove-softdelete');
-const {fastJoin} = require('feathers-hooks-common');
+const {fastJoin, paramsFromClient} = require('feathers-hooks-common');
 
 const joinsResolves = {
   joins: {
@@ -25,6 +25,16 @@ const joinsResolves = {
       const totalCurrentStatus = await knex.raw(query3).then((it) => it[0][0]);
 
       records.total_users_by_current_status = totalCurrentStatus;
+
+      const {my_team_details} = context.params;
+      if (my_team_details) {
+        const query = `select role_name, count(true) as total
+          from company_users
+          where company_id = ${records.id}
+          group by role_name`;
+
+        records.my_team_details = await knex.raw(query).then((it) => it[0][0]);
+      }
     },
   },
 };
@@ -32,7 +42,7 @@ const joinsResolves = {
 module.exports = {
   before: {
     all: [],
-    find: [searchAdminByQ()],
+    find: [searchAdminByQ(), paramsFromClient('my_team_details')],
     get: [],
     create: [],
     update: [],
